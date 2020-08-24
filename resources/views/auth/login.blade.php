82fd1f7a5f48565172ff10c8966e60bad1bf913f
asasdll <30594995+asasdll@users.noreply.github.com>
2020-08-24 16:09:43 +0700

    up_projert-24-8-63

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,26 +21,26 @@
 
 
     <title>{{  'leave' }}</title>
-      <link rel="shortcut icon" type="image/x-icon" href="{{ URL::to('/') }}/public/assets/img/icon_i2.png" />
+      <link rel="shortcut icon" type="image/x-icon" href="{{ URL::to('/') }}/assets/img/icon_i2.png" />
 
 
     <!-- // PLUGINS (css files) // -->
 
-    <link href="{{ URL::to('/') }}/public/assets/js/plugins/bootsnav_files/skins/color.css" rel="stylesheet">
+    <link href="{{ URL::to('/') }}/assets/js/plugins/bootsnav_files/skins/color.css" rel="stylesheet">
 
-    <link href="{{ URL::to('/') }}/public/assets/js/plugins/bootsnav_files/css/animate.css" rel="stylesheet">
+    <link href="{{ URL::to('/') }}/assets/js/plugins/bootsnav_files/css/animate.css" rel="stylesheet">
 
-    <link href="{{ URL::to('/') }}/public/assets/js/plugins/bootsnav_files/css/bootsnav.css" rel="stylesheet">
+    <link href="{{ URL::to('/') }}/assets/js/plugins/bootsnav_files/css/bootsnav.css" rel="stylesheet">
 
-    <link href="{{ URL::to('/') }}/public/assets/js/plugins/bootsnav_files/css/overwrite.css" rel="stylesheet">
+    <link href="{{ URL::to('/') }}/assets/js/plugins/bootsnav_files/css/overwrite.css" rel="stylesheet">
 
-    <link href="{{ URL::to('/') }}/public/assets/js/plugins/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link href="{{ URL::to('/') }}/assets/js/plugins/owl-carousel/owl.carousel.css" rel="stylesheet">
 
-    <link href="{{ URL::to('/') }}/public/assets/js/plugins/owl-carousel/owl.theme.css" rel="stylesheet">
+    <link href="{{ URL::to('/') }}/assets/js/plugins/owl-carousel/owl.theme.css" rel="stylesheet">
 
-    <link href="{{ URL::to('/') }}/public/assets/js/plugins/owl-carousel/owl.transitions.css" rel="stylesheet">
+    <link href="{{ URL::to('/') }}/assets/js/plugins/owl-carousel/owl.transitions.css" rel="stylesheet">
 
-    <link href="{{ URL::to('/') }}/public/assets/js/plugins/Magnific-Popup-master/Magnific-Popup-master/dist/magnific-popup.css" rel="stylesheet">
+    <link href="{{ URL::to('/') }}/assets/js/plugins/Magnific-Popup-master/Magnific-Popup-master/dist/magnific-popup.css" rel="stylesheet">
 
     <!--// ICONS //-->
 
@@ -52,9 +52,9 @@
 
     <!--// BOOTSTRAP & Main //-->
 
-    <link href="{{ URL::to('/') }}/public/assets/bootstrap-3.3.7/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ URL::to('/') }}/assets/bootstrap-3.3.7/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <link href="{{ URL::to('/') }}/public/assets/css/main.css" rel="stylesheet">
+    <link href="{{ URL::to('/') }}/assets/css/main.css" rel="stylesheet">
 
 </head>
 
@@ -116,7 +116,7 @@
 
                 <a class="navbar-brand" href="/">
 
-                    <img src="{{ URL::to('/') }}/public/assets/img/icon_i2.png" class="logo" alt="logo">
+                    <img src="{{ URL::to('/') }}/assets/img/icon_i2.png" class="logo" alt="logo">
 
                 </a>
 
@@ -433,7 +433,7 @@
 
                 <div class="footer-caption">
 
-                    <img src="{{ URL::to('/') }}/public/assets/img/icon_i2.png" class="img-responsive center-block" alt="logo">
+                    <img src="{{ URL::to('/') }}/assets/img/icon_i2.png" class="img-responsive center-block" alt="logo">
 
                     <hr>
 
@@ -540,22 +540,20 @@
 
     <!-- Include all compiled plugins (below), or include individual files as needed -->
 
-    <script src="{{ URL::to('/') }}/public/assets/bootstrap-3.3.7/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <script src="{{ URL::to('/') }}/assets/bootstrap-3.3.7/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
 
-    <script src="{{ URL::to('/') }}/public/assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+    <script src="{{ URL::to('/') }}/assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
 
-    <script src="{{ URL::to('/') }}/public/assets/js/plugins/bootsnav_files/js/bootsnav.js"></script>
+    <script src="{{ URL::to('/') }}/assets/js/plugins/bootsnav_files/js/bootsnav.js"></script>
 
-    <script src="{{ URL::to('/') }}/public/assets/js/plugins/typed.js-master/typed.js-master/dist/typed.min.js"></script>
+    <script src="{{ URL::to('/') }}/assets/js/plugins/typed.js-master/typed.js-master/dist/typed.min.js"></script>
 
     <script src="https://maps.googleapis.com/maps/api/js"></script>
 
-    <script src="{{ URL::to('/') }}/public/assets/js/plugins/Magnific-Popup-master/Magnific-Popup-master/dist/jquery.magnific-popup.js"></script>
+    <script src="{{ URL::to('/') }}/assets/js/plugins/Magnific-Popup-master/Magnific-Popup-master/dist/jquery.magnific-popup.js"></script>
 
-    <script src="{{ URL::to('/') }}/public/assets/js/main.js"></script>
+    <script src="{{ URL::to('/') }}/assets/js/main.js"></script>
 
 </body>
-
-
 
 </html>
